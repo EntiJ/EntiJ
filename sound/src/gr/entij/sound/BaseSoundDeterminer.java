59768879e5094b1c7ad57296ded2ab5d3bc5c02f
alieus <alieus@hotmail.gr>
2015-01-19 03:03:47 +0200

    Updated to support Object moves and not only long moves

--- sound/src/gr/entij/sound/BaseSoundDeterminer.java
+++ sound/src/gr/entij/sound/BaseSoundDeterminer.java
@@ -8,10 +8,10 @@ import gr.entij.event.StateEvent;
 public class BaseSoundDeterminer
 implements StateSoundDeterminer, MoveSoundDeterminer {
 
-    private final Map<Long, String> moveSounds = new HashMap<>();
+    private final Map<Object, String> moveSounds = new HashMap<>();
     private final Map<Long, String> stateSounds = new HashMap<>();
     
-    public void setSoundForMove(Long move, String soundFile) {
+    public void setSoundForMove(Object move, String soundFile) {
         moveSounds.put(move, soundFile);
     }
     
