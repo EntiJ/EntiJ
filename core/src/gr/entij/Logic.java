133956636e570ac647329d11123c32b63a1c0206
Χωριό <alieus@hotmail.gr>
2015-01-25 00:03:39 +0200

    Make Logic a component

--- core/src/gr/entij/Logic.java
+++ core/src/gr/entij/Logic.java
@@ -3,13 +3,13 @@ package gr.entij;
 /**
  * Judges moves supplied to an entity for their validity and, if they are
  * valid, decides a reaction. <p>
- * A {@code Logic} object is assigned to an entity via {@link Entity#setLogic(Logic)}
+ * A {@code Logic} object is assigned to an entity via {@link Entity#addLogic(Logic)}
  * and used by that entity every time {@link Entity#move} is called. <p>
  * The same {@code Logic} object can be assigned to multiple entities.
  * @see Entity#move
  * @see Entity#setLogic(Logic)
  */
-public interface Logic {
+public interface Logic extends Component {
 
     /**
      * Determines whether the given move is valid and, if it is valid, returns
@@ -23,4 +23,12 @@ public interface Logic {
      * invalid
      */
     MoveReaction reaction(Entity e, Object move);
+
+    /**
+     * Adds this logic to the given entity.
+     * @param target the entity to be added to
+     */
+    @Override default void attach(Entity target) {
+        target.addLogic(this);
+    }
 }
