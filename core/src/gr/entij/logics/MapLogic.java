d3863d321338b92167c2ac68b123792e8d58e46e
alieus <alieus@hotmail.gr>
2015-01-29 22:21:20 +0200

    Create gr.entij.logics.MapLogic

---
+++ core/src/gr/entij/logics/MapLogic.java
@@ -0,0 +1,112 @@
+package gr.entij.logics;
+
+import gr.entij.*;
+import java.util.*;
+import java.util.function.*;
+
+public class MapLogic implements Logic {
+    static final class Entry {
+        BiPredicate<Entity, Object> matcher;
+        BiFunction<Entity,  Object, MoveReaction> logic;
+        Entry next;
+
+        public Entry(BiPredicate<Entity, Object> matcher,
+                BiFunction<Entity, Object, MoveReaction> logic) {
+            this.matcher = matcher;
+            this.logic = logic;
+        }
+
+        MoveReaction applyMatching(Entity e, Object m) {
+            Entry current = this;
+            while (current != null) {
+                if (current.matcher == null || current.matcher.test(e, m)) {
+                    return current.logic.apply(e, m);
+                }
+                current = current.next;
+            }
+            return null;
+        }
+        
+        Entry findTail() {
+            Entry current ;
+            for (current = this; current.next != null; current = current.next);
+            return current;
+        }
+    }
+
+    private final Map<Object, Entry> map = new HashMap<>(5);
+    private Entry head;
+    private Entry tail;
+
+    @Override
+    public MoveReaction reaction(Entity e, Object move) {
+        Entry mapping = map.get(move); // search for the exact (or equal) move
+        if (mapping != null) {
+            return mapping.applyMatching(e, move);
+        }
+        mapping = map.get(move.getClass()); // search for the move's class
+        if (mapping != null) {
+            return mapping.applyMatching(e, move);
+        }
+
+        if (head != null) {
+            return head.applyMatching(e, move);
+        }
+        return null;
+    }
+
+    public MapLogic map(Object move, BiPredicate<Entity, Object> matcher,
+            BiFunction<Entity, Object, MoveReaction> logic) {
+        Objects.requireNonNull(move, "move cannot be null");
+        Objects.requireNonNull(logic, "logic cannot be null");
+        if (move.getClass() == Class.class)
+            throw new IllegalArgumentException("move cannot be of class Class");
+        
+        Entry entry = new Entry(matcher, logic);
+        Entry mapping = map.get(move);
+        if (mapping != null) {
+            mapping.findTail().next = entry;
+        } else {
+            map.put(move, entry);
+        }
+        return this;
+    }
+    public MapLogic map(Object move, BiFunction<Entity, Object, MoveReaction> logic) {
+        return map(move, null, logic);
+    }
+
+    public <T> MapLogic mapClass(Class<T> clazz, BiPredicate<Entity, T> matcher,
+            BiFunction<Entity, T, MoveReaction> logic) {
+        Objects.requireNonNull(clazz, "move cannot be null");
+        Objects.requireNonNull(logic, "logic cannot be null");
+        
+        Entry entry = new Entry((BiPredicate<Entity, Object>)matcher,
+                (BiFunction<Entity, Object, MoveReaction>)logic);
+        Entry mapping = map.get(clazz);
+        if (mapping != null) {
+            mapping.findTail().next = entry;
+        } else {
+            map.put(clazz, entry);
+        }
+        return this;
+    }
+    
+    public <T> MapLogic mapClass(Class<T> clazz, BiFunction<Entity, T, MoveReaction> logic) {
+        return mapClass(clazz, null, logic);
+    }
+    
+    public MapLogic match(BiPredicate<Entity, Object> matcher,
+            BiFunction<Entity, Object, MoveReaction> logic) {
+        Objects.requireNonNull(logic, "logic cannot be null");
+        
+        Entry entry = new Entry(matcher, logic);
+        if (tail == null) {
+            //assert head == null;
+            head = tail = entry;
+        } else {
+            tail.next = entry;
+            tail = entry;
+        }
+        return this;
+    }
+}
